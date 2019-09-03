578aae08a1ff3e88b8fdfc19a4a542a12dab974f
Andy <github@sockbot.com>
2019-09-03 16:08:22 -0700

    add selector function to get appointments for day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  const dayObj = state.days.filter(targetDay => targetDay.name === day)[0];
+  if (state.days.length === 0 || dayObj == undefined) {
+    return [];
+  }
+  const results = [];
+  dayObj.appointments.forEach(id => results.push(state.appointments[id]));
+  return results;
+}
