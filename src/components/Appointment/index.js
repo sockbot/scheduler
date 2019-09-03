ad57c69f95b7337f24c691de3f1babcf2385e315
Andy <github@sockbot.com>
2019-09-02 21:43:40 -0700

    create Appointment Booked view

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,21 @@
 import React from "react";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 import "components/Appointment/styles.scss";
+// import InterviewerList from "components/InterviewerList";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  const { id, time, interview } = props;
+  return (
+    <article className="appointment">
+      <Header key={id} time={time} />
+      {props.interview ? (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
