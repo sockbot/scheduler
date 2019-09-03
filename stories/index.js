ad686fe17191063e770d28fc647b8eae3b11a8ce
Andy <github@sockbot.com>
2019-09-02 21:21:43 -0700

    refactor stories and InterviewerLis to use value and onChange

--- stories/index.js
+++ stories/index.js
@@ -128,14 +128,14 @@ storiesOf("InterviewerList", module)
   .add("Initial", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("onChange")}
     />
   ))
   .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
-      setInterviewer={action("setInterviewer")}
+      value={3}
+      onChange={action("onChange")}
     />
   ));
 
