a9570105268f1bb69b787e13049853c6d2716159
Andy <github@sockbot.com>
2019-09-02 21:49:00 -0700

    add test appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,25 +3,80 @@ import DayList from "./DayList";
 
 import "components/Application.scss";
 
-export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
     }
-  ];
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Dave Chappelle",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "James Ehnes",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "11am",
+    interview: {
+      student: "Eddy Chen",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    }
+  }
+];
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0
+  }
+];
+
+export default function Application(props) {
   const [day, setDay] = useState("Monday");
 
   return (
