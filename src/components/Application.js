087d28923cf15653d993d11bfde3e2940d53c894
Andy <github@sockbot.com>
2019-09-03 21:59:05 -0700

    refactor to add interviewer data from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -10,23 +10,41 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = day => setState({ ...state, day });
   useEffect(() => {
-    Promise.all([axios.get("/api/days"), axios.get("/api/appointments")]).then(
-      all => {
-        const days = all[0].data;
-        const appointments = all[1].data;
-        setState(prev => ({ ...prev, days, appointments }));
-      }
-    );
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ])
+      .then(all => {
+        const [days, appointments, interviewers] = all;
+        setState(prev => ({
+          ...prev,
+          days: days.data,
+          appointments: appointments.data,
+          interviewers: interviewers.data
+        }));
+      })
+      .catch(err =>
+        console.error("ERROR IN APPLICATION COMPONENT", err.config)
+      );
     return;
   }, []);
 
   const appointmentsJSX = getAppointmentsForDay(state, state.day).map(
     appointment => {
-      return <Appointment key={appointment.id} {...appointment} />;
+      const interview = getInterview(state, appointment.interview);
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment}
+          interview={interview}
+        />
+      );
     }
   );
 
