1d2fb6279775ef4db7a17e63b002285ba8883485
Andy <github@sockbot.com>
2019-09-04 15:52:50 -0700

    refactor appointments to conditionally show different states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,18 +2,25 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   const { id, time, interview } = props;
   return (
     <article className="appointment">
       <Header key={id} time={time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === SHOW && (
         <Show student={interview.student} interviewer={interview.interviewer} />
-      ) : (
-        <Empty />
       )}
     </article>
   );
