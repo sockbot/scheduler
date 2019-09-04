88638bc60d590281f3f6473ba8fca38d7620bc2e
Andy <github@sockbot.com>
2019-09-03 21:57:18 -0700

    add getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,18 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const dayObj = state.days.filter(targetDay => targetDay.name === day)[0];
-  if (state.days.length === 0 || dayObj == undefined) {
+  if (state.days.length === 0 || dayObj === undefined) {
     return [];
   }
   const results = [];
   dayObj.appointments.forEach(id => results.push(state.appointments[id]));
   return results;
+}
+
+export function getInterview(state, interview) {
+  if (interview === null) {
+    return null;
+  }
+  const interviewObj = interview;
+  interviewObj.interviewer = state.interviewers[interviewObj.interviewer];
+  return interviewObj;
 }
