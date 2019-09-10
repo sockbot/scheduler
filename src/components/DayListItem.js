039c121965c57e23041376d9f40420bad219ea96
Andy <github@sockbot.com>
2019-09-10 16:53:52 -0700

    add cypress tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -21,7 +21,7 @@ export default function DayListItem(props) {
   });
 
   return (
-    <li onClick={() => setDay(name)} className={dayClass}>
+    <li data-testid="day" onClick={() => setDay(name)} className={dayClass}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
