97bb7f19b8bf8b9138c3c93fc5df9fa7d41dda1a
Andy <github@sockbot.com>
2019-09-09 17:13:58 -0700

    commit WIP

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment";
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  it("doesn't call the function", () => {
+    const fn = jest.fn();
+    expect(fn).toHaveBeenCalledTimes(0);
+  });
+});
